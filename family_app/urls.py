6824c6cdcf8c9e9bdca5c7bafaeb98ba5b7bc77d
abdou <abdoufaci982@gmail.com>
2020-06-26 17:48:36 +0100

    Initial Commit

--- family_app/urls.py
+++ family_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('shojo_search', views.shojo_search, name='shojo_search'),
     path('family_search', views.family_search, name='family_search'),
     path('', views.family, name='home'),
 ]
--- family_app/views.py
+++ family_app/views.py
@@ -21,3 +21,13 @@ def family_search(request):
     }
 
     return render(request, 'my_app/family_search.html', stuff_for_frontend)
+
+def shojo_search(request):
+    request.method
+    search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    stuff_for_frontend = {
+        'search': search,
+    }
+
+    return render(request, 'my_app/shojo_search.html', stuff_for_frontend)
