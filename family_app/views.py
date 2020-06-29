5ad6b027f283095a575f53615fc59763fc5916a5
abdou <abdoufaci982@gmail.com>
2020-06-29 20:33:06 +0100

    Initial Commit

--- family_app/views.py
+++ family_app/views.py
@@ -4,8 +4,6 @@ from django.shortcuts import render
 from requests.compat import quote_plus
 from . import models
 
-BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
-BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 
 def family(request):
