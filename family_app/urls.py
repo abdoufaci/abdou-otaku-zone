1c249421ae25a1f2947e36ad41fcd8dd3e4948e6
abdou <abdoufaci982@gmail.com>
2020-07-05 21:19:30 +0100

    Initial Commit

--- family_app/urls.py
+++ family_app/urls.py
@@ -2,6 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('blue_page4', views.blue_page4, name='blue_page4'),
+    path('blue_page3', views.blue_page3, name='blue_page3'),
+    path('blue_page2', views.blue_page2, name='blue_page2'),
+    path('blue_search', views.blue_search, name='blue_search'),
+    path('blue', views.blue, name='blue'),
+    path('green_page4', views.green_page4, name='green_page4'),
+    path('green_page3', views.green_page3, name='green_page3'),
+    path('green_page2', views.green_page2, name='green_page2'),
+    path('green_search', views.green_search, name='green_search'),
+    path('green', views.green, name='green'),
+    path('red_page4', views.red_page4, name='red_page4'),
+    path('red_page3', views.red_page3, name='red_page3'),
+    path('red_page2', views.red_page2, name='red_page2'),
+    path('red_search', views.red_search, name='red_search'),
+    path('red', views.red, name='red'),
     path('page4', views.page4, name='page4'),
     path('page3', views.page3, name='page3'),
     path('page2', views.page2, name='page2'),
--- family_app/views.py
+++ family_app/views.py
@@ -20,25 +20,99 @@ def family_search(request):
 
     return render(request, 'my_app/family_search.html', stuff_for_frontend)
 
+
 def shojo_search(request):
-
-
     return render(request, 'my_app/shojo_search.html',)
 
 
 def page2(request):
-
-
     return render(request, 'my_app/page2.html',)
 
 
 def page3(request):
-
-
     return render(request, 'my_app/page3.html',)
 
 
 def page4(request):
-
-
     return render(request, 'my_app/page4.html',)
+
+
+def red(request):
+    return render(request, 'base2.html',)
+
+
+def red_search(request):
+    request.method
+    search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    stuff_for_frontend = {
+        'search': search,
+    }
+
+    return render(request, 'my_app/search2.html', stuff_for_frontend)
+
+
+def red_page2(request):
+    return render(request, 'red/red_page2.html',)
+
+
+def red_page3(request):
+    return render(request, 'red/red_page3.html',)
+
+
+def red_page4(request):
+    return render(request, 'red/red_page4.html',)
+
+
+def green(request):
+    return render(request, 'green/base3.html',)
+
+
+def green_search(request):
+    request.method
+    search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    stuff_for_frontend = {
+        'search': search,
+    }
+
+    return render(request, 'green/search3.html', stuff_for_frontend)
+
+
+def green_page2(request):
+    return render(request, 'green/green_page2.html',)
+
+
+def green_page3(request):
+    return render(request, 'green/green_page3.html',)
+
+
+def green_page4(request):
+    return render(request, 'green/green_page4.html',)
+
+
+def blue(request):
+    return render(request, 'blue/base4.html',)
+
+
+def blue_search(request):
+    request.method
+    search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    stuff_for_frontend = {
+        'search': search,
+    }
+
+    return render(request, 'blue/search4.html', stuff_for_frontend)
+
+
+def blue_page2(request):
+    return render(request, 'blue/blue_page2.html',)
+
+
+def blue_page3(request):
+    return render(request, 'blue/blue_page3.html',)
+
+
+def blue_page4(request):
+    return render(request, 'blue/blue_page4.html',)
